bf4a3162e22fac56b44e82128456eb6328c8112d
Claudia Lam <claudiaslam@gmail.com>
2023-07-16 15:36:32 -0700

    add Pet Model

--- models.py
+++ models.py
@@ -14,3 +14,47 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.model):
+    """Pet Model. """
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name = db.Column(
+        db.String(25),
+        nullable=False
+    )
+
+    species = db.Column(
+        db.String(30),
+        nullable=False
+    )
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=' '
+    )
+
+    age = db.Column(
+        db.String(7),
+        nullable=False
+    )
+
+    notes = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    available = db.Column(
+        db.Boolean,
+        default=True,
+        nullable=False
+    )
